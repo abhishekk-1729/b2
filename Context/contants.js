a0b1b54161a4607c60a76dd35efc68998cd2cc3e
Abhishek Kumar <abhishekkumar@Abhisheks-Air-2.domain.name>
2024-06-04 00:35:52 +0530

    half deploy

---
+++ Context/contants.js
@@ -0,0 +1,6 @@
+// 0x5FbDB2315678afecb367f032d93F642f64180aa3
+
+import crowdFunding from "./CrowdFunding.json";
+//Crowd Funding MARKETPLACE
+export const CrowdFundingAddress = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
+export const CrowdFundingABI = crowdFunding.abi;
